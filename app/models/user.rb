df6ee8238849bbaaa561fa2c8ffc5a7e9d324cdb
SafwanMarwan <safwanmarwan11@gmail.com>
2015-07-10 16:23:20 +0800

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   validates :name, presence: true
+  validates :email, presence: true, format: { with: /\A\S+@\S+\.\S+\z/, message: 'invalid format' }
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
